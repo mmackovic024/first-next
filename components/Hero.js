e33a3d2959ed814ae6b2c25e0f2ca6d8f3c5d08e
mmackovic024 <mackovic.mirko@gmail.com>
2019-09-28 09:54:14 +0200

    fetch next launch and show details

--- components/Hero.js
+++ components/Hero.js
@@ -1,13 +1,31 @@
 export default function Hero() {
+  const [nextL, setNextL] = React.useState({});
+
+  React.useEffect(() => {
+    fetch('https://api.spacexdata.com/v3/launches/next')
+      .then(res => res.json())
+      .then(data => setNextL(data))
+      .catch(err => console.log(err));
+  }, []);
+
   return (
     <div className="hero">
-      <h1>HERO</h1>
+      <h2>Next launch</h2>
+      {'flight_number' in nextL && (
+        <>
+          <h3>#{nextL.flight_number}</h3>
+          <p>Mission: {nextL.mission_name}</p>
+          <p>Rocket: {nextL.rocket.rocket_name}</p>
+          <p>Launch site: {nextL.launch_site.site_name_long}</p>
+          <p>Scheduled date: {nextL.launch_date_local.split('T')[0]}</p>
+        </>
+      )}
       <style jsx>{`
         .hero {
           margin: 3rem auto;
-          padding: 2rem;
+          padding: 1rem;
           text-align: center;
-          width: 70%;
+          width: 60%;
           border: 1px solid #ccc;
           box-shadow: 1px 2px 4px lightgrey;
         }
