b7bb473d2cc5f65df6e61b3447241af06235ef37
mmackovic024 <mackovic.mirko@gmail.com>
2019-09-28 10:43:05 +0200

    details & styles

--- components/LaunchCard.js
+++ components/LaunchCard.js
@@ -4,16 +4,69 @@ const LaunchCard = ({ launch }) => {
   return (
     <Link href="/launch/[id]" as={`/launch/${launch.flight_number}`}>
       <div className="card">
-        <h2>Launch #{launch.flight_number}</h2>
-        <h3>Mission {launch.mission_name}</h3>
+        <div className="card-title">
+          <h3 className={launch.launch_success ? 'success' : 'fail'}>
+            Launch #{launch.flight_number}
+          </h3>
+        </div>
+        <div className="card-content">
+          <img src={launch.links.mission_patch} alt="patch" />
+          <div className="details">
+            <h3>Mission: {launch.mission_name}</h3>
+            <p>Launch site: {launch.launch_site.site_name_long}</p>
+            <p>Launch date: {launch.launch_date_local.split('T')[0]}</p>
+          </div>
+        </div>
         <style jsx>{`
           .card {
             cursor: pointer;
-            width: 31%;
+            width: calc((100% / 3) - 20px);
             height: 200px;
-            text-align: center;
-            border: 1px solid black;
             margin: 5px;
+            padding: 0.25rem;
+            background-color: #fff;
+            border: 1px solid #ccc;
+            box-shadow: 1px 2px 4px lightgrey;
+          }
+
+          .card-title {
+            width: 100%;
+            text-align: center;
+            border-bottom: 1px solid #ccc;
+          }
+
+          .card-content {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            align-items: center;
+            margin-top: 1rem;
+          }
+
+          img {
+            width: 30%;
+          }
+
+          .details {
+            width: 65%;
+            text-align: left;
+          }
+
+          .success {
+            color: green;
+          }
+
+          .fail {
+            color: red;
+          }
+
+          h3 {
+            margin: 0.4rem 0;
+          }
+
+          p {
+            margin: 0.25rem;
+            font-size: 0.925rem;
           }
         `}</style>
       </div>
